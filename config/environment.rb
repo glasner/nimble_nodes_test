5cbec4283933dcad7a4fbbcac9b5b83fd9182cdc
Jordan Glasner <jordan@digitalignition.com>
2010-03-03 19:48:55 -0500

    removed reference to old gem

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  require "#{File.dirname(__FILE__)}/../lib/nimble_nodes/lib/nimble_nodes"
+
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
